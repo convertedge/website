eada206197281fd2e16d5339f9a630e1600ff326
Nir Galon <nir@galons.io>
2025-03-06 13:33:15 +0200

    fix: add gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image", "@nuxt/ui", "@nuxthq/studio", "@nuxtjs/seo"],
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image", "@nuxt/ui", "@nuxthq/studio", "@nuxtjs/seo", "@zadigetvoltaire/nuxt-gtm"],
   runtimeConfig: {
     public: {
       posthogPublicKey: "phc_5QgLffNba5CNwWs5d9ag3zGLiptTe3Bxiq1MNcj9j0m",
       posthogHost: "https://us.i.posthog.com",
+      gtm: {
+        id: "GTM-NJJ9WMG9",
+      },
     },
   },
   ssr: true,
