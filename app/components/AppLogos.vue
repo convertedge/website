f92b2a6e359fc3ef16f0483ec8929fde4f41a57d
Nir Galon <nir@galons.io>
2025-03-06 13:52:27 +0200

    fix: switch logo

--- app/components/AppLogos.vue
+++ app/components/AppLogos.vue
@@ -4,7 +4,7 @@
       class="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5"
     >
       <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/logos/oracle.svg" alt="Oracle" width="158" height="48" />
-      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/logos/monday.svg" alt="Monday.com" width="158" height="48" />
+      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/logos/brex.svg" alt="Brex" width="158" height="48" />
       <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/logos/lightico.svg" alt="lightico" width="158" height="48" />
       <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="/logos/zencity.svg" alt="Zencity" width="158" height="48" />
       <img
